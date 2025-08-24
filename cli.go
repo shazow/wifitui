0e9559ec7f7ea10b7e7255ef220f6da75b74e99a
Andrey Petrov <andrey.petrov@shazow.net>
2025-08-23 17:07:10 -0700

    main: Add passphrase to show cli

--- cli.go
+++ cli.go
@@ -33,7 +33,7 @@ func runList(w io.Writer, verbose bool) error {
 
 	for _, c := range connections {
 		item := connectionItem{Connection: c}
-		fmt.Fprintf(w, "%s\t%s\n", item.plainTitle(), item.plainDescription())
+		fmt.Fprintf(w, "%s\t%s\n", item.Title(), item.Description())
 	}
 
 	return nil
@@ -50,9 +50,15 @@ func runShow(w io.Writer, verbose bool, ssid string) error {
 		return fmt.Errorf("failed to list networks: %w", err)
 	}
 
+	secret, err := backend.GetSecrets(ssid)
+	if err != nil {
+		return fmt.Errorf("failed to get network secret: %w", err)
+	}
+
 	for _, c := range connections {
 		if c.SSID == ssid {
 			fmt.Fprintf(w, "SSID: %s\n", c.SSID)
+			fmt.Fprintf(w, "Passphrase: %s\n", secret)
 			fmt.Fprintf(w, "Active: %t\n", c.IsActive)
 			fmt.Fprintf(w, "Known: %t\n", c.IsKnown)
 			fmt.Fprintf(w, "Secure: %t\n", c.IsSecure)
